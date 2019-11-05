980d1440675d1230b16448b8e217ca9421ad4548
got <got2536@hotmail.com>
2019-11-05 15:46:10 +0700

    Final

--- lib/widget/page1.dart
+++ lib/widget/page1.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:gotrist/models/pageholder_model.dart';
+import 'package:http/http.dart';
 
 class Page1 extends StatefulWidget {
   @override
@@ -6,8 +10,61 @@ class Page1 extends StatefulWidget {
 }
 
 class _Page1State extends State<Page1> {
+  // Explicit
+  List<String> tests = ['AA', 'BB', 'CC'];
+  List<PageHolderModel> pageHolderModel = [];
+
+  // Method
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    readAllData();
+  }
+
+  Future<void> readAllData() async {
+    String urlAPI = 'https://jsonplaceholder.typicode.com/posts';
+    Response response = await get(urlAPI);
+    var result = json.decode(response.body);
+    print('result = $result');
+
+    for (var map in result) {
+      PageHolderModel holderModel = PageHolderModel.fromJson(map);
+      String title = holderModel.title;
+      print('title = $title');
+
+      setState(() {
+        pageHolderModel.add(holderModel);
+      });
+    }
+  }
+
+  Widget showTitle(int index) {
+    return Text(pageHolderModel[index].title);
+  }
+
+  Widget showListView(int index) {
+    return Container(
+      padding: EdgeInsets.all(10.0),
+      decoration: index % 2 == 0
+          ? BoxDecoration(color: Colors.blue)
+          : BoxDecoration(color: Colors.red),
+      child: Column(
+        children: <Widget>[
+          showTitle(index),
+          Divider(),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Page1'),);
+    return ListView.builder(
+      itemCount: pageHolderModel.length,
+      itemBuilder: (BuildContext context, int index) {
+        return showListView(index);
+      },
+    );
   }
 }
